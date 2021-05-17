da710c54aa6dc5e81f9630825ad8f045c721c882
DESKTOP-RD8K6VP\jfasw <jfaswis2@gmail.com>
2021-05-17 02:12:46 +0200

    Funciones de la edicion del perfil

--- app/src/main/java/com/example/pildfarmaapp/activities/EditProfileActivity.java
+++ app/src/main/java/com/example/pildfarmaapp/activities/EditProfileActivity.java
@@ -1,20 +1,34 @@
 package com.example.pildfarmaapp.activities;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.example.pildfarmaapp.R;
 import com.example.pildfarmaapp.providers.AuthProvider;
 import com.example.pildfarmaapp.providers.ImageProvider;
 import com.example.pildfarmaapp.providers.UsersProvider;
+import com.example.pildfarmaapp.utils.FileUtil;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 import dmax.dialog.SpotsDialog;
 
@@ -111,5 +125,100 @@ public class EditProfileActivity extends AppCompatActivity {
     }
 
     private void clickEditProfile() {
+    }
+
+    private void selectOptionImage(final int numberImage) {
+
+        mBuilderSelector.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i == 0) {
+                    if (numberImage == 1) {
+                        openGallery(GALLERY_REQUEST_CODE_PROFILE);
+                    }
+                    else if (numberImage == 2) {
+                        openGallery(GALLERY_REQUEST_CODE_COVER);
+                    }
+                }
+                else if (i == 1){
+                    if (numberImage == 1) {
+                        takePhoto(PHOTO_REQUEST_CODE_PROFILE);
+                    }
+                    else if (numberImage == 2) {
+                        takePhoto(PHOTO_REQUEST_CODE_COVER);
+                    }
+                }
+            }
+        });
+
+        mBuilderSelector.show();
+
+    }
+
+    private void takePhoto(int requestCode) {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createPhotoFile(requestCode);
+            } catch(Exception e) {
+                Toast.makeText(this, "Hubo un error con el archivo " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+            if (photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(EditProfileActivity.this, "com.optic.socialmediagamer", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, requestCode);
+            }
+        }
+    }
+
+    private File createPhotoFile(int requestCode) throws IOException {
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File photoFile = File.createTempFile(
+                new Date() + "_photo",
+                ".jpg",
+                storageDir
+        );
+        if (requestCode == PHOTO_REQUEST_CODE_PROFILE) {
+            mPhotoPath = "file:" + photoFile.getAbsolutePath();
+            mAbsolutePhotoPath = photoFile.getAbsolutePath();
+        }
+        else if (requestCode == PHOTO_REQUEST_CODE_COVER) {
+            mPhotoPath2 = "file:" + photoFile.getAbsolutePath();
+            mAbsolutePhotoPath2 = photoFile.getAbsolutePath();
+        }
+        return photoFile;
+    }
+
+    private void openGallery(int requestCode) {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST_CODE_PROFILE && resultCode == RESULT_OK) {
+            try {
+                mPhotoFile = null;
+                mImageFile = FileUtil.from(this, data.getData());
+                mImageView.setImageBitmap(BitmapFactory.decodeFile(mImageFile.getAbsolutePath()));
+            } catch(Exception e) {
+                Log.d("ERROR", "Se produjo un error " + e.getMessage());
+                Toast.makeText(this, "Se produjo un error " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        if (requestCode == PHOTO_REQUEST_CODE_PROFILE && resultCode == RESULT_OK) {
+            mImageFile = null;
+            mPhotoFile = new File(mAbsolutePhotoPath);
+            Picasso.with(EditProfileActivity.this).load(mPhotoPath).into(mImageView);
+        }
+
+
     }
 }
